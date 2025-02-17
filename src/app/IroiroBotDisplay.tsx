0e7d4cea1f9c6fc42386c408667230995c67f85b
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2025-02-17 18:43:13 +0900

    add nostr-share-component

--- src/app/IroiroBotDisplay.tsx
+++ src/app/IroiroBotDisplay.tsx
@@ -1,4 +1,12 @@
 import { useEffect, useState } from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'nostr-share': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
 import iroiro from "./iroiroData/iroiro.json";
 import {
   Button,
